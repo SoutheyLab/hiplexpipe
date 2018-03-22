9279ea19b5e02a7b20cafe996659fd266c9494a4
Jason Steen <jason.steen@monash.edu>
2018-03-22 15:39:23 +1100

    modified characters that pasted badly from slack

--- src/pipeline.py
+++ src/pipeline.py
@@ -117,8 +117,8 @@ def make_pipeline_map(state):
 def make_pipeline_call(state):
     #this part of the pipeline will take the summary results of "map" and turn them into gatk and undr_rover vcfs
     pipeline = Pipeline(name='hiplexpipe')
-    with open("passed.sample.summary.txt", ‘r’) as inputf:
-        passed_files = inputf.read().split(‘\n’)
+    with open("passed.sample.summary.txt", 'r') as inputf:
+        passed_files = inputf.read().split('\n')
     stages = Stages(state)
 
     safe_make_dir('variants')
